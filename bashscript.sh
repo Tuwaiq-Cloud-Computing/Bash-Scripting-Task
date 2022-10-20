7367636acab2839b416a5a12c277be9baecd7ff0
Zaid Alsharif <zaidalshreef@gmail.com>
2022-10-20 15:32:41 +0300

    Add files via upload

--- bashscript.sh
+++ bashscript.sh
@@ -6,14 +6,21 @@ echo " $UID $USER"
 
 echo "$(whoami)"
 
-test "$(whoami)" = "yaman" && echo "yes" || echo "no"   
+if [ $? -ne 0 ] 
+then
+   echo "not success"
+else
+  echo "success"
+fi
+
+
+test "$(whoami)" = "yaman" && echo "yes" || echo "not yaman"   
 
 for i in {1..3}
 do
-   
-    read -p "enter user name: " name
+    read -p "Enter user name: " name
     read -p "EXPIRATION_DAY: " EXPIRATION_DAY
-    read -p "enter ROLE:" ROLE
+    read -p "Enter ROLE:" ROLE
     sudo useradd "$name" -c "$ROLE" -e $EXPIRATION_DAY;
     sudo passwd  "$name";
 done
